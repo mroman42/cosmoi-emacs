7512526b4a4e760b90456ccc56b8e10bbd1bc7bf
M42 <mromang08@gmail.com>
2017-02-21 18:14:47 +0100

    New packages

--- custom.el
+++ custom.el
@@ -15,6 +15,7 @@
  '(ido-mode (quote both) nil (ido))
  '(make-backup-files nil)
  '(menu-bar-mode nil)
+ '(op/theme (quote mdo-modified))
  '(org-latex-default-packages-alist
    (quote
     (("AUTO" "inputenc" t)
@@ -37,7 +38,7 @@
  '(org-startup-indented t)
  '(package-selected-packages
    (quote
-    (elpy intero company-auctex company ess ob-sagemath sage-mode ob-C ox-latex org-page sage-shell-mode org-plus-contrib org-drill-table idris-mode org-page engine-mode haskell-snippets yasnippet htmlize ox-reveal flycheck use-package auctex company-math rainbow-mode markdown-mode ox-twbs python-mode camcorder zenburn-theme crux haml-mode elmacro magit hlint-refactor multi-term org fsharp-mode haskell-mode)))
+    (ein elpy intero company-auctex company ess ob-sagemath sage-mode ob-C ox-latex org-page sage-shell-mode org-plus-contrib org-drill-table idris-mode org-page engine-mode haskell-snippets yasnippet htmlize ox-reveal flycheck use-package auctex company-math rainbow-mode markdown-mode ox-twbs python-mode camcorder zenburn-theme crux haml-mode elmacro magit hlint-refactor multi-term org fsharp-mode haskell-mode)))
  '(server-mode t)
  '(tool-bar-mode nil))
 (custom-set-faces
