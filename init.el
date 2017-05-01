87532f54f8b21853bcfe39119bd8486b237f6430
M42 <mromang08@gmail.com>
2017-05-01 20:23:51 +0200

    Uses https

--- init.el
+++ init.el
@@ -4,11 +4,11 @@
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
 (add-to-list 'package-archives
-	     '("org" . "http://orgmode.org/elpa/"))
+	     '("org" . "https://orgmode.org/elpa/"))
 (add-to-list 'package-archives
-	     '("gnu" . "http://elpa.gnu.org/packages/"))
+	     '("gnu" . "https://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives
-	     '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/"))
+	     '("melpa-stable" . "https://melpa-stable.milkbox.net/packages/"))
 
 
 ;; Added by Package.el.  This must come before configurations of
