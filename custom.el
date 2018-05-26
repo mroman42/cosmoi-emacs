f7b77b60d6d5d07c26c44cd8acb4567f0d3e3880
M42 <mromang08@gmail.com>
2018-05-26 14:03:02 +0200

    Custom.el: agenda files

--- custom.el
+++ custom.el
@@ -4,18 +4,17 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("7527f3308a83721f9b6d50a36698baaedc79ded9f6d5bd4e9a28a22ab13b3cb1" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "599f1561d84229e02807c952919cd9b0fbaa97ace123851df84806b067666332" default)))
+   '("7527f3308a83721f9b6d50a36698baaedc79ded9f6d5bd4e9a28a22ab13b3cb1" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "599f1561d84229e02807c952919cd9b0fbaa97ace123851df84806b067666332" default))
  '(electric-pair-mode t)
  '(engine-mode t)
  '(haskell-process-auto-import-loaded-modules t)
  '(haskell-process-log t)
  '(haskell-process-suggest-remove-import-lines t)
  '(haskell-stylish-on-save t)
- '(ido-mode (quote both) nil (ido))
+ '(ido-mode 'both nil (ido))
+ '(org-agenda-files '("~/Dropbox/orgzly/Tasks.org" "~/org/wiki/tasks.org"))
  '(org-latex-default-packages-alist
-   (quote
-    (("AUTO" "inputenc" t)
+   '(("AUTO" "inputenc" t)
      ("T1" "fontenc" t)
      ("" "fixltx2e" nil)
      ("" "graphicx" t)
@@ -28,16 +27,13 @@
      ("" "textcomp" t)
      ("" "amssymb" t)
      ("" "capt-of" nil)
-     ("colorlinks=true" "hyperref" nil))))
+     ("colorlinks=true" "hyperref" nil)))
  '(package-selected-packages
-   (quote
-    (base16-theme dracula-theme centered-window centered-window-mode nord-theme dashboard anzu avy smartparens-config smartparens volatile-highlights golden-ratio rainbow-delimiters better-defaults spacemacs-theme spaceline evil mediawiki visual-regexp polymode multiple-cursors flycheck-haskell-multi hs-lint flycheck-haskell flymake-haskell-multi flymake-hlint ox-gfm yankpad restart-emacs org-pomodoro which-key try counsel counsel-projectile projectile mastodon sx helm-google calfw smex graphviz-dot-mode google-translate-default-ui ob-translate define-word org-ref cdlatex org-gcal ein elpy intero company-auctex company ess ob-sagemath sage-mode ob-C ox-latex org-page sage-shell-mode org-plus-contrib org-drill-table idris-mode org-page engine-mode haskell-snippets yasnippet htmlize ox-reveal flycheck use-package auctex company-math rainbow-mode markdown-mode ox-twbs python-mode camcorder zenburn-theme crux haml-mode elmacro magit hlint-refactor multi-term org fsharp-mode haskell-mode)))
+   '(pdf-tools base16-theme dracula-theme centered-window centered-window-mode nord-theme dashboard anzu avy smartparens-config smartparens volatile-highlights golden-ratio rainbow-delimiters better-defaults spacemacs-theme spaceline evil mediawiki visual-regexp polymode multiple-cursors flycheck-haskell-multi hs-lint flycheck-haskell flymake-haskell-multi flymake-hlint ox-gfm yankpad restart-emacs org-pomodoro which-key try counsel counsel-projectile projectile mastodon sx helm-google calfw smex graphviz-dot-mode google-translate-default-ui ob-translate define-word org-ref cdlatex org-gcal ein elpy intero company-auctex company ess ob-sagemath sage-mode ob-C ox-latex org-page sage-shell-mode org-plus-contrib org-drill-table idris-mode org-page engine-mode haskell-snippets yasnippet htmlize ox-reveal flycheck use-package auctex company-math rainbow-mode markdown-mode ox-twbs python-mode camcorder zenburn-theme crux haml-mode elmacro magit hlint-refactor multi-term org fsharp-mode haskell-mode))
  '(safe-local-variable-values
-   (quote
-    ((eval setq org-latex-default-packages-alist
+   '((eval setq org-latex-default-packages-alist
            (cons
-            (quote
-             ("mathletters" "ucs" nil))
+            '("mathletters" "ucs" nil)
             org-latex-default-packages-alist))
      (org-latex-inputenc-alist
       ("utf8" . "utf8x"))
@@ -48,57 +44,43 @@
              (expand-file-name
               (locate-dominating-file buffer-file-name "UniMath"))))
            (setq fill-column 100)
-           (make-local-variable
-            (quote coq-use-project-file))
+           (make-local-variable 'coq-use-project-file)
            (setq coq-use-project-file nil)
-           (make-local-variable
-            (quote coq-prog-args))
+           (make-local-variable 'coq-prog-args)
            (setq coq-prog-args
-                 (\`
-                  ("-emacs" "-indices-matter" "-type-in-type" "-Q"
-                   (\,
-                    (concat unimath-topdir "UniMath"))
-                   "UniMath")))
-           (make-local-variable
-            (quote coq-prog-name))
+                 `("-emacs" "-indices-matter" "-type-in-type" "-Q" ,(concat unimath-topdir "UniMath")
+                   "UniMath"))
+           (make-local-variable 'coq-prog-name)
            (setq coq-prog-name
                  (concat unimath-topdir "sub/coq/bin/coqtop"))
-           (make-local-variable
-            (quote before-save-hook))
-           (add-hook
-            (quote before-save-hook)
-            (quote delete-trailing-whitespace))
+           (make-local-variable 'before-save-hook)
+           (add-hook 'before-save-hook 'delete-trailing-whitespace)
            (modify-syntax-entry 39 "w")
            (modify-syntax-entry 95 "w")
            (if
                (not
-                (memq
-                 (quote agda-input)
-                 features))
+                (memq 'agda-input features))
                (load
                 (concat unimath-topdir "emacs/agda/agda-input")))
            (if
                (not
                 (member
-                 (quote
-                  ("chimney" "╝"))
+                 '("chimney" "╝")
                  agda-input-user-translations))
                (progn
                  (setq agda-input-user-translations
                        (cons
-                        (quote
-                         ("chimney" "╝"))
+                        '("chimney" "╝")
                         agda-input-user-translations))
                  (setq agda-input-user-translations
                        (cons
-                        (quote
-                         ("==>" "⟹"))
+                        '("==>" "⟹")
                         agda-input-user-translations))
                  (agda-input-setup)))
            (set-input-method "Agda"))
      (encoding . utf-8)
      (eval text-scale-increase 1)
-     (eval text-scale-adjust)))))
+     (eval text-scale-adjust))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
